55ff987c8cbaa029d971fdcc83bdd74ed2689483
Srinivas P <srinivas2006.srini@gmail.com>
2026-02-09 01:33:34 +0530

    v1

--- m1/avg3.c
+++ m1/avg3.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main() 
 {
-    float diameter, radius, perimeter;
-    printf("Enter the diameter of the circle: ");
-    scanf("%f", &diameter);
-    radius = diameter / 2;
-    perimeter = 2 * 3.14159 * radius;  
-    printf("Radius of the circle: %.2f\n", radius);
-    printf("Perimeter (Circumference) of the circle: %.2f\n", perimeter);
+    int a,b,c;
+    float avg;
+    printf("Enter the three numbers: \n");
+    scanf("%d%d%d",&a&b&c);
+    avg = a+b+c;
+    printf("Average of three numbers = %d\n",avg);
     return 0;
 }
