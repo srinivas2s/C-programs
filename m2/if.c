bd9642068043a4307184332f707625d59590c23e
Srinivas P <srinivas2006.srini@gmail.com>
2026-02-09 04:21:01 +0530

    v1

---
+++ m2/if.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int num = 10;
+    if (num > 0) 
+        printf("Number is positive\n");
+    return 0;
+}
---
+++ m2/if_else.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int num = -5;
+    if (num > 0) 
+        printf("Number is positive\n");
+    else 
+        printf("Number is negative or zero\n");
+    return 0;
+}
