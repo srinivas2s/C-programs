cd5b06cf5b55803fa5d5627019c9d619a2352704
Srinivas P <srinivas2006.srini@gmail.com>
2026-02-09 05:47:57 +0530

    v2

--- m2/imp.c/imp.c
+++ m2/imp.c/imp.c
@@ -21,3 +21,22 @@ int main() {
     }
     return 0;
 }
+
+/* Program Name: Check Voting Eligibility */
+
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if (age >= 18) {
+        printf("Eligible to vote");
+    } else {
+        printf("Not eligible to vote");
+    }
+
+    return 0;
+}
