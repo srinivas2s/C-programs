470b8cab46214a623f80a35a421597f54beb7e51
Srinivas P <srinivas2006.srini@gmail.com>
2026-02-09 05:45:33 +0530

    v1

---
+++ m2/imp.c/imp.c
@@ -0,0 +1,23 @@
+/*Develop a C program to print numbers from 1 to 10 using for loop*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+ /*Develop a C program to print "VVCE" 10 times using for loop.*/
+
+ #include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("VVCE\n");
+    }
+    return 0;
+}
